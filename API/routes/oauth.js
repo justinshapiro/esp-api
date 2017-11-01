f6f1c3b2924ce13f0c704600673cad0825ced15e
Justin Shapiro <justin.shapiro@ucdenver.edu>
2017-11-01 17:25:56 -0600

    Refactor authentication endpoint, added OAuth endpoint stubs.

---
+++ API/routes/oauth.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const responder = require('./httpRouteResponder');
+
+exports.authorize = function(req, res, next) {
+	// TODO
+};
+
+exports.token = function(req, res, next) {
+	// TODO
+};
