c7db04e19a4681fec9af50732326a5aad506a4fe
Justin Shapiro <justin.shapiro@ucdenver.edu>
2017-11-10 01:51:08 -0700

    Changed GCP deployment parameters

--- API/database/db-connection.js
+++ API/database/db-connection.js
@@ -7,12 +7,12 @@ module.exports = require('knex')({
 	connection: {
 		// How to set host:
 		//  - 127.0.0.1 for local testing
-		//  - 35.194.32.254 to locally use hosted db on Cloud SQL (may not work if you have the Cloud SQL proxy running)
-		//  - /cloudsql/esp-mobile-182605:us-central1:esp-db to deploy to App Engine (production)
+		//  - 104.197.189.152 to locally use hosted db on Cloud SQL (may not work if you have the Cloud SQL proxy running)
+		//  - /cloudsql/esp-mobile-182605:us-central1:esp to deploy to App Engine (production)
 		host : '127.0.0.1',
 		// user, password, and database remain unchanged regardless of local, develop or production scenarios
 		user: 'postgres',
 		password: 'seniordesign',
-		database: 'esp_db'
+		database: 'postgres'
 	}
 });
