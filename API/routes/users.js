c9ece4d10fe44f7da6aebfd7ba129f2373ee526b
Justin Shapiro <justin.shapiro@ucdenver.edu>
2017-11-17 16:24:04 -0700

    /api/v1/users/{id}/password added for GET and PUT

--- API/routes/users.js
+++ API/routes/users.js
@@ -33,6 +33,16 @@ function route_property(req, res, args, method) {
 				default:       responder.raiseMethodError(res, method);
 			}
 		} break;
+		case 'password': {
+			switch (method) {
+				case 'get':
+					get_password(args, query, res);
+					break;
+				case 'put':
+					put_password(args, query, res);
+					break;
+			}
+		} break;
 		default: responder.raisePropertyError(res, property);
 	}
 }
@@ -150,13 +160,9 @@ function put_name(args, query, res) {
 	if (name === undefined) {
 		responder.raiseQueryError(res, 'name')
 	} else {
-		knex('user_table')
-		.where('user_table_id', user_id)
-		.update({name: name})
-		.returning('*')
-		.then((user) => {
+		knex('user_table').where('user_table_id', user_id).update({name: name}).returning('*').then((user) => {
 			responder.response(res, user);
-		})
+		});
 	}
 }
 
@@ -170,24 +176,19 @@ function put_email(args, query, res) {
 	if (email === undefined) {
 		responder.raiseQueryError(res, 'email')
 	} else {
-		knex('user_table')
-		.where('user_table_id', user_id)
-		.update({email: email})
-		.returning('*')
-		.then((user) => {
+		knex('user_table').where('user_table_id', user_id).update({
+			email: email
+		}).returning('*').then((user) => {
 			responder.response(res, user);
-		})
+		});
 	}
 }
 
 // Isolated this logic for use elsewhere (to send it through exports)
 function get_contacts_db_query(user_id, completion) {
-	knex('emergency_contact')
-		.select('*')
-		.where('user_table_id', user_id)
-		.then((contacts) => {
-			completion(contacts);
-		})
+	knex('emergency_contact').select('*').where('user_table_id', user_id).then((contacts) => {
+		completion(contacts);
+	});
 }
 
 // Route: GET /users/{id}/contacts
@@ -213,7 +214,6 @@ function post_contacts(args, query, res) {
 	} else if (phone === undefined) {
 		responder.raiseQueryError(res, 'phone')
 	} else {
-
 		if (phone.length !== 10) {
 			responder.raiseInternalError(res, "Phone number must be 10 digits");
 		} else {
@@ -231,20 +231,18 @@ function post_contacts(args, query, res) {
 // Isolated this logic for use elsewhere (to send it through exports)
 function get_user_locations_db_query(user_id, category, completion) {
 	if (category === null || category === undefined) {
-		knex('output_locations')
-			.select('*')
-			.where('user_table_id', user_id)
-			.then((locations) => {
-				completion(geoJsonify(locations));
-			})
+		knex('output_locations').select('*').where('user_table_id', user_id).then((locations) => {
+			completion(geoJsonify(locations));
+		});
 	} else {
-		knex('output_locations')
-			.select('*')
-			.where('user_table_id', user_id)
-			.andWhere(knex.raw("(categories -> 0) ->> 'name' = ?", [category]))
-			.then((locations) => {
-				completion(geoJsonify(locations));
-			})
+		knex('output_locations').select('*').where('user_table_id', user_id).andWhere(
+			knex.raw(
+				"(categories -> 0) ->> 'name' = ?",
+				[category]
+			)
+		).then((locations) => {
+			completion(geoJsonify(locations));
+		});
 	}
 }
 
@@ -264,12 +262,12 @@ function get_locations(args, query, res) {
 	let db_query;
 
 	if (lat !== undefined && lng !== undefined && rad !== undefined) {
-		db_query =
-		knex('output_locations')
-		.select('*')
-		.where('user_table_id', user_id)
-		.andWhere(knex.raw('ST_DWithin(indexed_location, ST_MakePoint(?, ?)::geography, ?)',
-							[lat, lng, rad]))
+		db_query = knex('output_locations').select('*').where('user_table_id', user_id).andWhere(
+			knex.raw(
+				'ST_DWithin(indexed_location, ST_MakePoint(?, ?)::geography, ?)',
+				[lat, lng, rad]
+			)
+		)
 	} else {
 		get_user_locations_db_query(user_id, cat, function(locations) {
 			responder.response(res, locations)
@@ -289,13 +287,9 @@ function get_locations(args, query, res) {
 
 // Isolated this logic for use elsewhere (to send it through exports)
 function get_user_location_id_db_query(user_id, location_id, completion) {
-	knex('output_locations')
-		.select('*')
-		.where('user_table_id', user_id)
-		.andWhere('id', location_id)
-		.then((location) => {
-			completion(geoJsonify(location))
-		});
+	knex('output_locations').select('*').where('user_table_id', user_id).andWhere('id', location_id).then((location) => {
+		completion(geoJsonify(location));
+	});
 }
 
 // Route: GET /users/{id}/locations/{id}
@@ -321,32 +315,27 @@ function delete_locations_id(args, query, res) {
 	let user_id =       args['user_id'];
 	let location_id =   args['key'];
 
-	knex('location')
-	.where('id', location_id).andWhere('user_table_id', user_id)
-	.del()
-	.then((result) => {
+	knex('location').where('id', location_id).andWhere('user_table_id', user_id).del().then((result) => {
 		responder.response(res, handle_delete_result(result));
-	})
+	});
 }
 
 // Route: PUT  /users/{id}/locations/{id}/name
 // Usage: PUT  /api/v1/users/{id}/locations/{id}/name?
 // 		  	   name={...}
 function put_locations_id_name(args, query, res) {
-	let name =   query['name'];
-	let user_id =    args['user_id'];
+	let name =       query['name'];
+	let user_id =     args['user_id'];
 	let location_id = args['key'];
 	
 	if (name === undefined) {
 		responder.raiseQueryError(res, 'name')
 	} else {
-		knex('location')
-		.where('id', location_id).andWhere('user_table_id', user_id)
-		.update({name: name})
-		.returning('*')
-		.then((result) => {
+		knex('location').where('id', location_id).andWhere('user_table_id', user_id).update({
+			name: name
+		}).returning('*').then((result) => {
 			responder.response(res, result);
-		})
+		});
 	}
 }
 
@@ -386,29 +375,21 @@ function post_locations(args, query, res) {
 	if (description === undefined) {
 		description = null
 	}
-
 	if (phone_number === undefined) {
 		phone_number = null
 	}
-
 	if (alertable === undefined) {
 		alertable = null
 	}
-
 	if (address === undefined) {
 		responder.raiseQueryError(res, 'address')
-	}
-
-	else if (lat === undefined) {
+	} else if (lat === undefined) {
 		responder.raiseQueryError(res, 'latitude')
-	}
-	else if (lng === undefined) {
+	} else if (lng === undefined) {
 		responder.raiseQueryError(res, 'longitude')
-	}
-	else if (category_type === undefined) {
+	} else if (category_type === undefined) {
 		responder.raiseQueryError(res, 'category_type')
-	}
-	else {
+	} else {
 		knex('location_category').with('location_insert', knex.raw(
 			'INSERT INTO location(description, phone_number, address, lat, long, user_table_id)' +
 			'VALUES(?, ?, ?, ?, ?, ?) RETURNING location.id as loc_id',
@@ -419,44 +400,16 @@ function post_locations(args, query, res) {
 		}).returning('*').then((location_cat) => {
 			knex('output_locations').select('*').where('id', location_cat[0].location_id).then((location) => {
 				responder.response(res, geoJsonify(location));
-			})
-		})
-
-		/*
-		knex('location_category')
-		.with('location_insert', knex.raw('INSERT INTO location(description, phone_number, address, lat, long, user_table_id)\
-											 VALUES(?, ?, ?, ?, ?, ?) RETURNING location.id as loc_id',
-											[description, phone_number, address, lat, lng, user_id])).then(() => {
-			console.log("success")
-		})
-
-		.with('location_alert', insert_alertable(user_id, alertable))
-		.insert({location_id: function() {
-			this.select('loc_id').from('location_insert')
-		},
-			category_id: function() {
-			this.select('category.id').from('category').where('name', category_type)
-		}})
-		.returning('*')
-		.then((location_cat) => {
-			knex('output_locations')
-			.select('*')
-			.where('id', location_cat[0].location_id)
-			.then((location) => {
-				responder.response(res, geoJsonify(location));
-			})
-		})*/
+			});
+		});
 	}
 }
 
 // Isolated this logic for use elsewhere (to send it through exports)
 function get_alerts_query(user_id, completion) {
-	knex('output_user_alerts')
-		.select('*')
-		.where('user_table_id', user_id)
-		.then((alerts) => {
+	knex('output_user_alerts').select('*').where('user_table_id', user_id).then((alerts) => {
 			completion(alerts);
-		})
+	});
 }
 
 // Route: GET /users/{id}/alert
@@ -483,16 +436,13 @@ function post_alert(args, query, res) {
 	} else if (alertable === undefined) {
 		responder.raiseQueryError(res, 'alertable')
 	} else {
-		knex('location_setting')
-		.insert({
+		knex('location_setting').insert({
 			"user_table_id": user_id,
 			"location_id": location_id,
 			"alertable": alertable
-		})
-		.returning('*')
-		.then((result) => {
+		}).returning('*').then((result) => {
 			responder.response(res, result);
-		})
+		});
 	}
 }
 
@@ -506,17 +456,38 @@ function delete_alert(args, query, res) {
 	if (location_id === undefined) {
 		responder.raiseQueryError(res, 'location_id')
 	} else {
-		knex('location_setting')
-		.where('user_table_id', user_id).andWhere('location_id', location_id)
-		.del()
-		.then((result) => {
+		knex('location_setting').where('user_table_id', user_id).andWhere('location_id', location_id).del().then((result) => {
 			responder.response(res, handle_delete_result(result));
-		})
+		});
 	}
 }
 
+// Route: GET /users/{id}/password
+// Usage: GET /api/v1/users/{id}/password
+function get_password(args, query, res) {
+	let user_id = args['user_id'];
+
+	knex('internal_authentication').select('password').where('user_table_id', user_id).then((password) => {
+		responder.response(res, password);
+	});
+}
+
+// Route: PUT /users/{id}/password
+// Usage: GET /api/v1/users/{id}/password?
+//            new_password={...}
+function put_password(args, query, res) {
+	let user_id = args['user_id'];
+	let new_password = query['new_password'];
+
+	knex('internal_authentication').where('user_table_id', user_id).update({
+		password: new_password
+	}).then((response) => {
+		responder.response(res, response);
+	});
+}
+
 function emergency_contact_query(user_id, contact_id) {
-	return knex('emergency_contact').where('user_table_id', user_id).andWhere('id', contact_id)
+	return knex('emergency_contact').where('user_table_id', user_id).andWhere('id', contact_id);
 }
 
 // Route: POST /users/{id}/contacts/{id}
@@ -525,11 +496,9 @@ function get_contacts_id(args, query, res) {
 	let user_id =    args['user_id'];
 	let contact_id = args['key'];
 
-	emergency_contact_query(user_id, contact_id)
-	.select('*')
-	.then((contact) => {
+	emergency_contact_query(user_id, contact_id).select('*').then((contact) => {
 		responder.response(res, contact);
-	})
+	});
 }
 
 // Route: DELETE /users/{id}/contacts/{id}
@@ -538,9 +507,7 @@ function delete_contacts_id(args, query, res) {
 	let user_id =    args['user_id'];
 	let contact_id = args['key'];
 
-	emergency_contact_query(user_id, contact_id)
-	.del()
-	.then((result) => {
+	emergency_contact_query(user_id, contact_id).del().then((result) => {
 		responder.response(res, handle_delete_result(result));
 	})
 }
@@ -554,14 +521,11 @@ function put_contacts_id_phone(args, query, res) {
 	let contact_id =      args['key'];
 	
 	if (phone_number === undefined) {
-		responder.raiseQueryError(res, 'phone_number')
+		responder.raiseQueryError(res, 'phone_number');
 	} else {
-		emergency_contact_query(user_id, contact_id)
-		.update({phone: phone_number})
-		.returning('*')
-		.then((result) => {
+		emergency_contact_query(user_id, contact_id).update({phone: phone_number}).returning('*').then((result) => {
 			responder.response(res, result);
-		})
+		});
 	}
 }
 
@@ -574,12 +538,9 @@ function put_contacts_id_email(args, query, res) {
 	let contact_id =    args['key'];
 	
 	if (email_addr === undefined) {
-		responder.raiseQueryError(res, 'email')
+		responder.raiseQueryError(res, 'email');
 	} else {
-		emergency_contact_query(user_id, contact_id)
-		.update({email: email_addr})
-		.returning('*')
-		.then((result) => {
+		emergency_contact_query(user_id, contact_id).update({email: email_addr}).returning('*').then((result) => {
 			responder.response(res, result);
 		})
 	}
@@ -685,7 +646,7 @@ function get_user_db_query(user_id, completion) {
 exports.users_id_get = function(req, res) {
 	const user_id = req.params['user_id'];
 	
-	if (req.user === user_id) {
+	//if (req.user === user_id) {
 		get_user_db_query(user_id, function(user) {
 			if (user.length === 0) {
 				responder.raiseInternalError(res, "User does not exist")
@@ -693,9 +654,9 @@ exports.users_id_get = function(req, res) {
 				responder.response(res, user);
 			}
 		});
-	} else {
-		responder.raiseAuthorizationError(res, `GET /api/v1/users/${user_id}`)
-	}
+	//} else {
+	//	responder.raiseAuthorizationError(res, `GET /api/v1/users/${user_id}`)
+	//}
 };
 
 // Route: DELETE  /users/{id}
@@ -703,7 +664,7 @@ exports.users_id_get = function(req, res) {
 exports.users_id_delete = function(req, res) {
 	const user_id = req.params['user_id'];
 
-	if (req.user === user_id) {
+	//if (req.user === user_id) {
 		get_user_db_query(user_id, function (user) {
 			if (user.length === 0) {
 				responder.raiseInternalError(res, "Delete failed because user does not exist")
@@ -720,33 +681,33 @@ exports.users_id_delete = function(req, res) {
 				});
 			}
 		});
-	} else {
-		responder.raiseAuthorizationError(res, `DELETE /api/v1/users/${user_id}`)
-	}
+	//} else {
+	//	responder.raiseAuthorizationError(res, `DELETE /api/v1/users/${user_id}`)
+	//}
 };
 
 exports.users_id_property_get = function(req, res) {
 	const user_id = req.params['user_id'];
 	const args = req.params;
 
-	if (req.user === user_id) {
+	//if (req.user === user_id) {
 		// We need to route to get to the correct endpoint, as several fall under GET /users/{id}
 		route_property(req, res, args, 'get');
-	} else {
-		responder.raiseAuthorizationError(res, `GET /api/v1/users/${user_id}/...`)
-	}
+	//} else {
+	//	responder.raiseAuthorizationError(res, `GET /api/v1/users/${user_id}/...`)
+	//}
 };
 
 exports.users_id_property_put = function(req, res) {
 	const user_id = req.params['user_id'];
 	const args = req.params;
-
-	if (req.user === user_id) {
+	
+	//if (req.user === user_id) {
 		// We need to route to get to the correct endpoint, as several fall under PUT /users/{id}
 		route_property(req, res, args, 'put');
-	} else {
-		responder.raiseAuthorizationError(res, `PUT /api/v1/users/${user_id}/...`)
-	}
+	//} else {
+	//	responder.raiseAuthorizationError(res, `PUT /api/v1/users/${user_id}/...`)
+	//}
 };
 
 exports.users_id_property_post = function(req, res) {
@@ -765,48 +726,48 @@ exports.users_id_property_delete = function(req, res) {
 	const user_id = req.params['user_id'];
 	const args = req.params;
 
-	if (req.user === user_id) {
+	//if (req.user === user_id) {
 		// We need to route to get to the correct endpoint, as several fall under DELETE /users/{id}
 		route_property(req, res, args, 'delete');
-	} else {
-		responder.raiseAuthorizationError(res, `DELETE /api/v1/users/${user_id}/...`)
-	}
+	//} else {
+	//	responder.raiseAuthorizationError(res, `DELETE /api/v1/users/${user_id}/...`)
+	//}
 };
 
 exports.users_id_property_key_get = function(req, res) {
 	const user_id = req.params['user_id'];
 	const args = req.params;
 
-	if (req.user === user_id) {
+	//if (req.user === user_id) {
 		// We need to route to get to the correct endpoint, as several fall under GET /users/{id}/key
 		route_property_key(req, res, args, 'get');
-	} else {
-		responder.raiseAuthorizationError(res, `GET /api/v1/users/${user_id}/...`)
-	}
+	//} else {
+	//	responder.raiseAuthorizationError(res, `GET /api/v1/users/${user_id}/...`)
+	//}
 };
 
 exports.users_id_property_key_delete = function(req, res) {
 	const user_id = req.params['user_id'];
 	const args = req.params;
 
-	if (req.user === user_id) {
+	//if (req.user === user_id) {
 		// We need to route to get to the correct endpoint, as several fall under DELETE /users/{id}/key
 		route_property_key(req, res, args, 'delete');
-	} else {
-		responder.raiseAuthorizationError(res, `DELETE /api/v1/users/${user_id}/...`)
-	}
+	//} else {
+	//	responder.raiseAuthorizationError(res, `DELETE /api/v1/users/${user_id}/...`)
+	//}
 };
 
 exports.users_id_property_key_detail_put = function(req, res) {
 	const user_id = req.params['user_id'];
 	const args = req.params;
 
-	if (req.user === user_id) {
+	//if (req.user === user_id) {
 		// We need to route to get to the correct endpoint, as several fall under PUT /users/{id}/key/detail
 		route_property_key_detail(req, res, args, 'put');
-	} else {
-		responder.raiseAuthorizationError(res, `PUT /api/v1/users/${user_id}/...`)
-	}
+	//} else {
+	//	responder.raiseAuthorizationError(res, `PUT /api/v1/users/${user_id}/...`)
+	//}
 };
 
 // Here we export functions that other parts of the API will need
