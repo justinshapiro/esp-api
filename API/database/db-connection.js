15feeb4a9bd3641aec6fad32c343fd4bb9554cbd
Justin Shapiro <justin.shapiro@ucdenver.edu>
2017-11-25 03:20:15 -0700

    Updated POST /notification to account for recent changes

--- API/database/db-connection.js
+++ API/database/db-connection.js
@@ -10,10 +10,10 @@ module.exports = require('knex')({
 		//  - 35.202.119.248 to locally use hosted db on Cloud SQL (may not work if you have the Cloud SQL proxy running)
 		//  - /cloudsql/esp-mobile-182605:us-central1:esp-db to deploy to App Engine (production)
 		//  - change 'database' to 'postgres' when deploying
-		host : '/cloudsql/esp-mobile-182605:us-central1:esp-db',
+		host : '127.0.0.1',
 		// user, password, and database remain unchanged regardless of local, develop or production scenarios
 		user: 'postgres',
 		password: 'seniordesign',
-		database: 'postgres'
+		database: 'esp_db'
 	}
 });
